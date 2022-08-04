5bc9cb9dcf8fc5c825b58c13b0797231db34bb28
LucaSk-x <lucapiccinno64@gmail.com>
2022-08-04 17:35:34 +0200

    creazione rotte + index

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+Route::get('/movies', 'MovieController@index')->name('movies.index');
+Route::get('/movies/create', 'MovieController@create')->name('movies.create');
+Route::post('/movies', 'MovieController@store')->name('movies.store');
+Route::get('/movies/{movie}', 'MovieController@show')->name('movies.show');
+
+
+Route::get('/movies/{movie}/edit', 'MovieController@edit')->name('movies.edit');
+Route::patch('/movies/{movie}', 'MovieController@update')->name('movies.update');
+Route::delete('/movies/{movie}', 'MovieController@destroy')->name('movies.destroy');
